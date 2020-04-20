c1b6d13229698e04e40912fc6bd7d1695ca687d7
Shaq1256 <lukaszmioduszewskii@gmail.com>
2020-04-20 13:34:20 +0100

    commit 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://https://mighty-chamber-07682.herokuapp.com/v1/task/';
+  var apiRoot = 'https://mighty-chamber-07682.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
